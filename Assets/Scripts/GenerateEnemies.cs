9c5623b0cab5c7f01e64b46c73d0dea2788c26b0
OmarMWarraich <earthling986@gmail.com>
2023-02-25 20:27:12 +0500

    fix: Design

--- Assets/Scripts/GenerateEnemies.cs
+++ Assets/Scripts/GenerateEnemies.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 
 public class GenerateEnemies : MonoBehaviour
 {
-     public GameObject theEnemy;
-     
-     public int enemiesCreated;
-     
+    public GameObject theEnemy;
+
+    public int enemiesCreated;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(CreateEnemies());
     }
-    
+
     IEnumerator CreateEnemies() {
-     	int numberOfEnemies = 10;
-     	float yPosition = 0.4f;
-     	
-     	while (enemiesCreated < numberOfEnemies) {
-     	
-     		var xPosition = Random.Range(-25,25);	
-     		var zPosition = Random.Range(-25,25);
-     		
-     		
-     		Instantiate(theEnemy, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
-     		
-     		yield return new WaitForSeconds(0.1f);
-     		
-     		enemiesCreated += 1;	
-     	}
-     	
+        int numberOfEnemies = 10;
+        float yPosition = 0.4f;
+
+        while (enemiesCreated < numberOfEnemies) {
+
+            var xPosition = Random.Range(-25, 25);
+            var zPosition = Random.Range(-25, 25); 
+
+            Instantiate(theEnemy, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.1f);
+
+            enemiesCreated += 1;
+
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{	
+{
     CharacterController CharacterControllerPlayer;
-    
+
     float Speed = 3f;
-    
+
     float pitch = 0f;
-    
+
     public Transform cameraTransform;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,38 +21,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	MovePlayer();
-	
-	Look();
+        MovePlayer();
+
+        Look();
     }
-    
+
     void MovePlayer() {
-    	
-    	Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    	
-    	move = Vector3.ClampMagnitude(move, 1f);
-    	
-    	move = transform.TransformDirection(move);
-    	
-    	CharacterControllerPlayer.SimpleMove(move * Speed);
-    	}
-    	
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        move = transform.TransformDirection(move);
+
+        CharacterControllerPlayer.SimpleMove(move * Speed);
+
+    }
+
     float Sensitivity = 3f;
-    
+
     float MinPitch = -45f;
-    
+
     float MaxPitch = 45f;
-    	
+
     void Look() {
-    
-    	float mouseX = Input.GetAxis("Mouse X") * Sensitivity;
-    	
-    	transform.Rotate(0, mouseX, 0);
-    	
-    	pitch -= Input.GetAxis("Mouse Y") * Sensitivity;
-    	
-    	pitch = Mathf.Clamp(pitch, MinPitch, MaxPitch);
-    	
-    	cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
+
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity;
+
+        transform.Rotate(0, mouseX, 0);
+
+        pitch -= Input.GetAxis("Mouse Y") * Sensitivity;
+
+        pitch = Mathf.Clamp(pitch, MinPitch, MaxPitch);
+
+        cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
+
     }
 }
