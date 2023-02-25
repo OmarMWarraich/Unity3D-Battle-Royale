6d5e8e5e3b88aeceb347c04c89bbd27b4f4a761e
OmarMWarraich <earthling986@gmail.com>
2023-02-25 18:38:19 +0500

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{	
+    CharacterController CharacterControllerPlayer;
+    
+    float Speed = 3f;
+    
+    float pitch = 0f;
+    
+    public Transform cameraTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterControllerPlayer = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	MovePlayer();
+	
+	Look();
+    }
+    
+    void MovePlayer() {
+    	
+    	Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    	
+    	move = Vector3.ClampMagnitude(move, 1f);
+    	
+    	move = transform.TransformDirection(move);
+    	
+    	CharacterControllerPlayer.SimpleMove(move * Speed);
+    	}
+    	
+    float Sensitivity = 3f;
+    
+    float MinPitch = -45f;
+    
+    float MaxPitch = 45f;
+    	
+    void Look() {
+    
+    	float mouseX = Input.GetAxis("Mouse X") * Sensitivity;
+    	
+    	transform.Rotate(0, mouseX, 0);
+    	
+    	pitch -= Input.GetAxis("Mouse Y") * Sensitivity;
+    	
+    	pitch = Mathf.Clamp(pitch, MinPitch, MaxPitch);
+    	
+    	cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
+    }
+}
